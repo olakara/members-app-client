401c85ae5dc4d8ee24e8f73a8bb5f61a5dee45b1
Abdel Raoof <olakara@gmail.com>
2022-06-18 14:47:41 +0400

    adding observable and gateway

---
+++ shared/observable.js
@@ -0,0 +1,29 @@
+
+export default class Observable {
+  _value = null;
+  observers = [];
+  parent = {};
+
+  constructor(parent) {
+    this.value = parent;
+  }
+
+  set value(newValue) {
+    this._value = newValue;
+  }
+
+  get value() {
+    return this._value;
+  }
+
+  subscribe = func => {
+    this.observers.push(func);
+    this.notify();
+  };
+
+  notify = () => {
+    this.observers.forEach(observer => {
+      observer(this._value);
+    });
+  };
+}
