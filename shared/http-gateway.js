22981d37809d103ed74cd2fa96a8fc80ff6afc50
Abdel Raoof <olakara@gmail.com>
2022-06-21 20:05:27 +0400

    chore: setting up shared scripts and styles

--- shared/http-gateway.js
+++ shared/http-gateway.js
@@ -1,21 +1,42 @@
 class HttpGateway {
-  get = async path => {
-    const response = await fetch(path);
+  get = async url => {
+
+    const requestOptions = {
+        method: "GET",
+        headers: this.authHeader(url)
+    };
+    const response = await fetch(url, requestOptions);
     const dto = response.json();
     return dto;
   };
 
-  post = async (path, requestDto) => {
-    const response = await fetch(path, {
+  post = async (url, requestDto) => {
+    const response = await fetch(url, {
       method: "POST",
       body: JSON.stringify(requestDto),
       headers: {
         "Content-Type": "application/json"
       }
     });
+   
+    if(response.ok) {        
+        console.log('response ok')
+    }else    
+        console.log('response not ok');
     const responseDto = response.json();
     return responseDto;
   };
+
+
+  authHeader = (url) => {
+        
+        const token = localStorage.getItem('token');
+        if (token) {
+            return { Authorization: `Bearer ${token}` };
+        } else {
+            return {};
+        }
+    }
 }
 
 const httpGateway = new HttpGateway();
