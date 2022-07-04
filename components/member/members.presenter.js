cffef43ca551e73ee67e843df8f4976b04c6eaf0
Abdel Raoof <olakara@gmail.com>
2022-07-04 20:47:15 +0400

    feat(members): add skeleton for members presenter

---
+++ components/member/members.presenter.js
@@ -0,0 +1,13 @@
+import membersRepostory from "./members.repository";
+
+export default class MembersPresenter {
+
+    load = async (callback) => {
+
+    }
+
+    createMember = async (memberDto, successCallback, errorCallback) => {
+
+        
+    }
+}
