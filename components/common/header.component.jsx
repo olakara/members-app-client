c28d21e8bbf443ef82576df179130d75c41c04e2
Abdel Raoof <olakara@gmail.com>
2022-07-04 20:18:17 +0400

    fix: logout link's cursor not showing as pointer

--- components/common/header.component.jsx
+++ components/common/header.component.jsx
@@ -47,13 +47,13 @@ export default function HeaderComponent() {
                          </div>
                         <div className="mt-5"> 
                         {isLoggedIn && (<Link href="/logout" className="cursor-pointer">
-                            
-                            <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24"
-                            stroke="currentColor" strokeWidth="1">
-                            <path strokeLinecap="round" strokeLinejoin="round"
-                                d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1" />
-                            </svg>
-                            
+                            <a>
+                                <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24"
+                                stroke="currentColor" strokeWidth="1">
+                                <path strokeLinecap="round" strokeLinejoin="round"
+                                    d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1" />
+                                </svg>
+                            </a>
                         </Link>)} 
                         </div>
                     </div>
