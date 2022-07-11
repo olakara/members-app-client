759b4c3c8181860fa23cd5d131dfe6c8f9117d46
Abdel Raoof <olakara@gmail.com>
2022-07-11 15:58:46 +0400

    feat(add member): building the form WIP

--- shared/constants.js
+++ shared/constants.js
@@ -1,15 +1,13 @@
-import { getURL } from "next/dist/shared/lib/utils";
-
 const prod = {
-   BASE_URL: 'http://membership-app.me/',
-   USER_INFO: 'users/me',
-   SIGN_IN: 'users/sign-in'
+  BASE_URL: 'http://membership-app.me/',
+  USER_INFO: 'users/me',
+  SIGN_IN: 'users/sign-in',
 };
 
 const dev = {
-   BASE_URL: 'http://localhost:5000/',
-   USER_INFO: 'users/me',
-   SIGN_IN: 'users/sign-in'
+  BASE_URL: 'http://localhost:5000/',
+  USER_INFO: 'users/me',
+  SIGN_IN: 'users/sign-in',
 };
 
 export const config = process.env.NODE_ENV === 'development' ? dev : prod;
