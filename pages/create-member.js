d925a31a2dd9d370589965233f215330ba7404a8
Abdel Raoof <olakara@gmail.com>
2022-08-11 20:06:26 +0400

    fix: target report using rdlc

--- pages/create-member.js
+++ pages/create-member.js
@@ -264,8 +264,8 @@ export default function CreateMemberPage() {
 
   const handleDownload = async (event) => {
     event.preventDefault();
-    //await memberPresenter.downloadReceipt(memberId, membershipId);
-    Router.push({ pathname: '/receipt', query: { id: memberId } });
+    await memberPresenter.downloadReceipt(memberId, membershipId);
+    //Router.push({ pathname: '/receipt', query: { id: memberId } });
   };
 
   const onSelectingEmiratesIdFront = async (event) => {
