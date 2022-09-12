38ffa7a14f80e92423b2734d64ea14d083fe5a36
Abdel Raoof <olakara@gmail.com>
2022-09-12 20:05:42 +0400

    fix: button alignment issue in home page for mobiles

--- components/common/action-button.component.jsx
+++ components/common/action-button.component.jsx
@@ -4,7 +4,7 @@ function ActionButtonComponent(props) {
   return (
     <a
       href={action}
-      className="inline-flex items-center px-2.5 py-1.5 border border-transparent font-medium rounded text-green-700 bg-green-100 hover:bg-green-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500 mx-2"
+      className="inline-flex items-center px-2.5 py-1.5 border border-transparent font-medium rounded text-green-700 bg-green-100 hover:bg-green-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500 m-2"
     >
       {children}
     </a>
