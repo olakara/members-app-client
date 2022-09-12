918c7aa8560e646f6285793e5088f1a0bb2fbbbf
Abdel Raoof <olakara@gmail.com>
2022-09-12 16:07:24 +0400

    feat: show dispute reject and approve button only if applicable user is viewing the dispute

--- pages/view-dispute/[id].js
+++ pages/view-dispute/[id].js
@@ -305,27 +305,31 @@ function ViewDisputePage() {
                   Cancel
                 </button>
 
-                <button
-                  id="reject-button"
-                  title="Reject"
-                  type="button"
-                  onClick={handleReject}
-                  disabled={dispute.status || (!formData.justification && !dispute.isCanApprove)}
-                  className="ml-3 inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white disabled:bg-gray-500 bg-red-600 hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500"
-                >
-                  Reject
-                </button>
+                {dispute.isCanApprove && (
+                  <>
+                    <button
+                      id="reject-button"
+                      title="Reject"
+                      type="button"
+                      onClick={handleReject}
+                      disabled={dispute.status || (!formData.justification && !dispute.isCanApprove)}
+                      className="ml-3 inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white disabled:bg-gray-500 bg-red-600 hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500"
+                    >
+                      Reject
+                    </button>
 
-                <button
-                  id="approve-button"
-                  title="Approve"
-                  type="button"
-                  onClick={handleApprove}
-                  disabled={dispute.status || (!formData.justification && !dispute.isCanApprove)}
-                  className="ml-3 inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white disabled:bg-gray-500 bg-green-600 hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500"
-                >
-                  Approve
-                </button>
+                    <button
+                      id="approve-button"
+                      title="Approve"
+                      type="button"
+                      onClick={handleApprove}
+                      disabled={dispute.status || (!formData.justification && !dispute.isCanApprove)}
+                      className="ml-3 inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white disabled:bg-gray-500 bg-green-600 hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500"
+                    >
+                      Approve
+                    </button>
+                  </>
+                )}
               </div>
             </div>
           </div>
